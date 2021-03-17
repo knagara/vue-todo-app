590e31fa0d170107a6c38f0e5893baeaef50bd6a
knagara <nagara.keita@gmail.com>
2021-03-17 17:44:22 +0900

    Create Input, List

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        <h1>TODOアプリ</h1>   
+    <div id="todo-app">
+        <h1>TODOアプリ</h1> 
+        <Input></Input>
+        <List></List>  
     </div>
 </template>
+
+<script>
+import Input from './Input'
+import List from './List'
+
+export default {
+    components: {
+        Input,
+        List
+    }
+}
+</script>
+
+<style scoped>
+#todo-app {
+    width: 400px;
+    margin: 2rem auto;
+    text-align: center;
+}
+</style>
